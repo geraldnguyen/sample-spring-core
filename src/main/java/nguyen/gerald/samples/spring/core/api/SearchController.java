1dec78b11d9aa88a032b64871bf42a8cbdab5c93
Gerald Nguyen <ngthehuy@gmail.com>
2023-01-28 22:28:20 +0800

    /search/?query endpoint

--- src/main/java/nguyen/gerald/samples/spring/core/api/SearchController.java
+++ src/main/java/nguyen/gerald/samples/spring/core/api/SearchController.java
@@ -2,6 +2,7 @@ package nguyen.gerald.samples.spring.core.api;
 
 import nguyen.gerald.samples.spring.core.service.search.SearchService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ public class SearchController {
     @Autowired
     private SearchService searchService;
 
+    @GetMapping
     public String[] search(@RequestParam("query") String query) {
         return searchService.search(query);
     }
