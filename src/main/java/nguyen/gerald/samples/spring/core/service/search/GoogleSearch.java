1cdd138eb154f15e1b8b54efdebaf1c0a93aef57
Gerald Nguyen <ngthehuy@gmail.com>
2023-01-28 22:30:40 +0800

    Apply @Primary to one of the bean

--- src/main/java/nguyen/gerald/samples/spring/core/service/search/GoogleSearch.java
+++ src/main/java/nguyen/gerald/samples/spring/core/service/search/GoogleSearch.java
@@ -1,7 +1,9 @@
 package nguyen.gerald.samples.spring.core.service.search;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
+@Primary
 @Service
 public class GoogleSearch implements SearchService {
     @Override
